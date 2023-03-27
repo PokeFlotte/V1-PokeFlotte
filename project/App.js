f9bf9e796d6cf3725238751908dccc9948c306e1
eliseeLeydier <elisee.leydier@etu.univ-amu.fr>
2023-03-27 19:36:14 +0200

    ajout welcome

--- project/App.js
+++ project/App.js
@@ -1,74 +1,91 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, ImageBackground } from 'react-native';
 
-export default function App() {
-  const [count, setCount] = useState(0);
-  const [progress, setProgress] = useState(0);
-  const MAX_PROGRESS = 5;
-  const [progressWidth, setProgressWidth] = useState('0%');
+const backgroundImage = require('../assets/back.png');
 
-  const handlePress = () => {
-    if (count < MAX_PROGRESS) {
-      setCount(count + 1);
-      const newProgress = progress + 1;
-      setProgress(newProgress);
-      const width = `${(newProgress / MAX_PROGRESS) * 100}%`;
-      setProgressWidth(width);
-    }
-  };
-
+export default function MyPage() {
   return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={handlePress}>
-        <Text style={styles.text}>J'ai bu!</Text>
-      </TouchableOpacity>
-      {count === MAX_PROGRESS ? (
-        <Text style={styles.counter}>Félicitations, vous avez atteint votre objectif journalier. Vous avez bu {MAX_PROGRESS} verres.</Text>
-      ) : (
-        <Text style={styles.counter}>Plus que {MAX_PROGRESS - count} verres</Text>
-      )}
-      <View style={styles.progressContainer}>
-        <View style={[styles.progress, { width: progressWidth }]} />
+    <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputLabel}>Comment t'appelles-tu ?</Text>
+          <TextInput style={styles.textInput} />
+        </View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputLabel}>Quel est votre poids ?</Text>
+          <View style={styles.weightInputContainer}>
+            <TextInput style={styles.weightInput} />
+            <Text style={styles.weightUnit}>Kg</Text>
+          </View>
+        </View>
+        <TouchableOpacity style={styles.startButton}>
+          <Text style={styles.startButtonText}>start</Text>
+        </TouchableOpacity>
       </View>
-      <Text style={styles.counter}>Nombre de clics: {count}</Text>
-    </View>
+    </ImageBackground>
   );
 }
 
-
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
+  backgroundImage: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    resizeMode: 'cover',
   },
-  button: {
-    backgroundColor: 'blue',
-    borderRadius: 50,
-    width: 100,
-    height: 50,
-    alignItems: 'center',
+  container: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    backgroundColor: '#C80D0D',
+    borderWidth: 2,
+    borderColor: '#C80D0D',
+    borderRadius: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
     marginBottom: 20,
   },
-  text: {
+  inputLabel: {
     color: 'white',
-    fontSize: 16,
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
   },
-  counter: {
-    fontSize: 20,
-  },
-  progressContainer: {
-    width: '30%',
-    height: 20,
+  textInput: {
+    backgroundColor: 'white',
+    borderColor: 'black',
+    borderWidth: 5,
     borderRadius: 10,
-    borderWidth: 1,
-    borderColor: 'blue',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
   },
-  progress: {
-    height: '100%',
-    borderRadius: 8,
-    backgroundColor: 'blue',
+  weightInputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  weightInput: {
+    backgroundColor: 'white',
+    borderColor: 'black',
+    borderWidth: 5,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginRight: 10,
+  },
+  weightUnit: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  startButton: {
+    backgroundColor: '#C80D0D',
+    borderRadius: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  startButtonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
