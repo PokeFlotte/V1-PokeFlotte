ce61a4640d819b96b4743e7a21410eae6bfb1c28
calliouxes <elisee.leydier@icloud.com>
2023-04-03 19:03:52 +0200

    pokemon image gagne

--- project/Home.js
+++ project/Home.js
@@ -1,26 +1,30 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image, Modal } from 'react-native';
 import users from "./users.json";
+import userPokemon from "./users-pokemons.json";
+import Pokemon from "./Pokemon.json";
+
 const user = users[1];
 
 export default function Home({ navigation }) {
-    const [count, setCount] = useState(user.nbVerres); 
+
+
+
+    const [count, setCount] = useState(user.nbVerres);
     const [progress, setProgress] = useState(0);
     const MAX_PROGRESS = user.objectif;
-    const [progressWidth, setProgressWidth] = useState((100*user.nbVerres)/MAX_PROGRESS + '%');
+    const [progressWidth, setProgressWidth] = useState((100 * user.nbVerres) / MAX_PROGRESS + '%');
     const [showModal, setShowModal] = useState(false);
 
     const handlePress = () => {
-    console.log(count, MAX_PROGRESS)
-    if (count < MAX_PROGRESS) {
-        setCount(count + 1);
-        user.nbVerres = user.nbVerres + 1;
-        const newProgress = user.nbVerres;
-        setProgress(newProgress);
-        const width = `${(newProgress / MAX_PROGRESS) * 100}%`;
-        setProgressWidth(width);
-        //forceUpdate();
-    }
+        if (count < MAX_PROGRESS) {
+            setCount(count + 1);
+            user.nbVerres = user.nbVerres + 1;
+            const newProgress = user.nbVerres;
+            setProgress(newProgress);
+            const width = `${(newProgress / MAX_PROGRESS) * 100}%`;
+            setProgressWidth(width);
+        }
     };
 
     const showPopup = () => {
@@ -43,12 +47,63 @@ export default function Home({ navigation }) {
             return require("../assets/pokeBall.png");
         }
     }
-    function isFullMsg() {
-    if (user.nbVerres >= user.objectif) {
-        return `Bravo vous avez gagné un pokémon. Vous avez bu ${user.nbVerres} verres.`;
-    } else {
-        return `Vous n'avez pas atteint votre objectif. Vous avez bu ${user.nbVerres} verres.`;
+    function afficherId(id) {
+        const idStr = id.toString(); // Convertir l'ID en chaîne de caractères
+        if (idStr.length === 1) {
+            return `00${idStr}`;
+        }
+        if (idStr.length === 2) {
+            return `0${idStr}`;
+        }
+        return idStr;
     }
+
+    function reset() {
+
+        if (user.nbVerres >= user.objectif) {
+            setCount(0);
+            user.nbVerres = 0;
+            const newProgress = 0;
+            setProgress(newProgress);
+            const width = `0%`;
+            setProgressWidth(width);
+        }
+
+        setShowModal(false)
+        return;
+    }
+
+    function isFullMsg() {
+        if (user.nbVerres >= user.objectif) {
+            const pokemonChoisi = Pokemon[afficherId(aleatoire(valeurExclus()))];
+            return (
+                <View>
+                    <Text style={styles.popupText}>{`Bravo tu as gagné ${pokemonChoisi.name}!`}</Text>
+
+                    <Image source={require(`./../assets/pokemon/${pokemonChoisi.image}`)} style={styles.popupImage} resizeMode="contain" />
+
+                </View>
+            );
+        } else {
+            return <Text style={styles.popupText}>{`Vous n'avez pas atteint votre objectif. Vous avez bu ${user.nbVerres} verres.`}</Text>;
+        }
+    }
+
+    function valeurExclus() {
+        var valExclus = [];
+        for (let i = 0; i < userPokemon['1'].pokemons.length; i++) {
+            valExclus.push(userPokemon['1'].pokemons[i]);
+        }
+        return valExclus;
+    }
+
+    function aleatoire(exclus) {
+        var valAleatoire = Math.floor(Math.random() * 151) + 1;
+        if (exclus.includes(valAleatoire)) {
+            aleatoire(exclus);
+        } else {
+            return valAleatoire;
+        }
     }
 
     return (
@@ -81,7 +136,7 @@ export default function Home({ navigation }) {
                 <View style={styles.popup}>
                     <Text style={styles.popupTitle}>Nouveau Pokémon ?</Text>
                     <Text style={styles.popupText}>{isFullMsg()}</Text>
-                    <TouchableOpacity onPress={() => setShowModal(false)} style={styles.popupButton}>
+                    <TouchableOpacity onPress={reset} style={styles.popupButton}>
                         <Text style={styles.popupButtonText}>Fermer</Text>
                     </TouchableOpacity>
                 </View>
@@ -188,13 +243,13 @@ const styles = StyleSheet.create({
         backgroundColor: '#0CC0DF',
     },
     popup: {
-        backgroundColor: '#fff',
+        backgroundColor: "#fff",
         borderRadius: 20,
         padding: 20,
-        alignSelf: 'center',
-        width: '80%',
-        marginTop: '50%',
-        shadowColor: '#000',
+        alignSelf: "center",
+        width: "80%",
+        marginTop: "50%",
+        shadowColor: "#000",
         shadowOffset: {
             width: 0,
             height: 2,
@@ -202,6 +257,12 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.25,
         shadowRadius: 4,
         elevation: 5,
+    },
+    popupImage: {
+        width: 150,
+        height: 150,
+        borderRadius: 10,
+        marginBottom: 10,
     },
     popupTitle: {
         fontSize: 24,
